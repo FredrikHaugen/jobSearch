76adbd2a272f115c1b4174eae708706ff65d18e7
fredrikhaugen <fredrik@haugendesign.net>
2023-09-18 13:07:01 +0200

    Initial commit

---
+++ components/home/nearby/nearbyjobs.style.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../../../constants";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: SIZES.xLarge,
+  },
+  logoImage: {
+    width: "70%",
+    height: "70%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: SIZES.small,
+  },
+  headerTitle: {
+    fontSize: SIZES.large,
+    fontFamily: FONT.medium,
+    color: COLORS.primary,
+  },
+  headerBtn: {
+    fontSize: SIZES.medium,
+    fontFamily: FONT.medium,
+    color: COLORS.gray,
+  },
+  cardsContainer: {
+    marginTop: SIZES.medium,
+    gap: SIZES.small,
+  },
+});
+
+export default styles;
